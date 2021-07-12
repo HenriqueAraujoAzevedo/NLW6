50d0481a905dafda26ef3bb2088a1674e9f4eed5
Henrique Araujo Azevedo <henrique.a.azevedo@hotmail.com>
2021-07-12 18:57:43 -0300

    NLW 6 Aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
 
 //@types/express
 const app = express();
 
-/**
- * GET    - Buscar uma informação
- * POST   - Inserir criar  uma informação
- * PUT    - Alterar uma informação 
- * DELETE - Remover uma informação
- * PATCH  - Alterar uma informação especifica(senha, avatar)
- */
-
-app.get("/test", (request, response) => {
-    //request - entrada
-    //response - saida
-    return response.send("Olá Mundo GET")
-});
-app.post("/test-post", (request, response) => {
-    return response.send("Olá Mundo POST")
-});
-
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"))
 
